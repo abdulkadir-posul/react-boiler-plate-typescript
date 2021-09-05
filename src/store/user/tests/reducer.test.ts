ed3173078e916630b0d11e07261c96932752b328
abdulkadirposul <a.posul@sevensenders.com>
2021-09-05 22:04:36 +0200

    user reducer test initial value test updated

--- src/store/user/tests/reducer.test.ts
+++ src/store/user/tests/reducer.test.ts
@@ -4,17 +4,17 @@ describe('user reducer', () => {
     it('returns the initial state', () => {
         expect(reducer(undefined, {})).toEqual({
             logged: false,
-            token: null,
-            refreshToken: null,
-            roles: null,
-            error: null,
+            token: "",
+            refreshToken: true,
+            roles: [],
+            error: "",
             filters: {
                 dateFrom: '',
                 dateTo: '',
                 id: '',
                 pickUpStations: [],
-                dropOffStations: []
-            }
+                dropOffStations: [],
+            },
         });
     });
 });
